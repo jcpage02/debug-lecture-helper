88464bfb8a40918a45ad0d2add5d0241a752a617
James Page <jcpage02@outlook.com>
2019-01-09 12:19:15 -0700

    fixed add message

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const session = require('express-session');
 
 const app = express();
 
+app.use(express.json())
+
 app.use(
   session({
-    secret: SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
   })
@@ -21,6 +23,7 @@ app.use((req, res, next) => {
       let regex = new RegExp(badWords[i], 'g');
       req.body.message = req.body.message.replace(regex, '****');
     }
+    next()
   } else {
     next();
   }
@@ -30,6 +33,6 @@ app.get('/api/messages', messagesCtrl.getAllMessages);
 app.get('/api/messages/history', messagesCtrl.history);
 app.post('/api/messages', messagesCtrl.createMessage);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}.`);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server listening on port ${process.env.SERVER_PORT}.`);
 });
